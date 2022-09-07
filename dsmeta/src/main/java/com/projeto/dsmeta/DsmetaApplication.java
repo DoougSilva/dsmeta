729c9ff28558c210c954a11d769f886576b3993b
douglas <douglas.silva@projuris.com.br>
2022-09-07 14:58:54 -0300

    security config

--- dsmeta/src/main/java/com/projeto/dsmeta/DsmetaApplication.java
+++ dsmeta/src/main/java/com/projeto/dsmeta/DsmetaApplication.java
@@ -1,4 +1,4 @@
-package com.project.dsmeta;
+package com.projeto.dsmeta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- dsmeta/src/test/java/com/projeto/dsmeta/DsmetaApplicationTests.java
+++ dsmeta/src/test/java/com/projeto/dsmeta/DsmetaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.project.dsmeta;
+package com.projeto.dsmeta;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
